d62d09f192c72e7ff80b90617a37b94a0b9b5311
EdenOvad <edenovad0@gmail.com>
2024-05-19 16:06:55 +0300

    update the api request routers buy run_simulation with local_run and update simulation action from table

--- simulation-management-v1/backend/app/controllers/simulator.py
+++ simulation-management-v1/backend/app/controllers/simulator.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI, Request, APIRouter
-from simulator.simulator import simulate_flood_dns
-from simulator.job_generator import gen_ddp_pairs
+from flooddns.external.simulation.main import local_run
+# from flooddns.external.jobs_generator.main import gen_ddp_pairs_different_sizes
 from models.simulation import SimulationPayload, TemporyPayload
 import os
 import pandas as pd
 import subprocess
 from schemas.simulation import deleteSimulation, popSimulation, insertSimulation, update_simulation_db, popResults
 from datetime import datetime
+import random
 from bson.objectid import ObjectId
+from flooddns.external.schemas.routing import Routing
 
 router = APIRouter()
 
@@ -15,44 +17,57 @@ router = APIRouter()
 @router.post("/simulate_flood_dns")
 async def run_simulation(request: Request):
     payload = await request.json()
+    seed = random.choice([0, 42, 200, 404, 1234])
     payload_data = SimulationPayload(
         simulation_name=payload['params']['simulation_name'],
         num_jobs=payload['params']['num_jobs'],
         num_tors=payload['params']['num_tors'],
-        num_cores=payload['params']['num_cores'],
+        n_cores=payload['params']['num_cores'],
         ring_size=payload['params']['ring_size'],
         routing=payload['params']['routing'],
-        path=payload['params']['path']
+        path=payload['params']['path'],
+        seed=seed
     )
     simulation_data = TemporyPayload(
         simulation_name=payload['params']['simulation_name'],
         num_jobs=payload['params']['num_jobs'],
         num_tors=payload['params']['num_tors'],
-        num_cores=payload['params']['num_cores'],
+        n_cores=payload['params']['num_cores'],
         ring_size=payload['params']['ring_size'],
         routing=payload['params']['routing'],
-        path=payload['params']['path']
+        path=payload['params']['path'],
+        seed=seed
     )
-    if payload_data.ring_size == 1:
-        payload_data.ring_size = 2
-    traffic = await gen_ddp_pairs(
-        accelerator_name="AWS_TRAINIUM_V1",
-        n_tors=payload_data.num_tors,
-        num_concurrent_jobs=payload_data.num_jobs,
-        data_parallelism_dim=payload_data.ring_size
-    )
-    result = await simulate_flood_dns(
+    result = await local_run(
         num_jobs=payload_data.num_jobs,
         num_tors=payload_data.num_tors,
-        num_cores=payload_data.num_cores,
+        n_cores=payload_data.n_cores,
         ring_size=payload_data.ring_size,
-        routing="ecmp"
+        routing=payload_data.routing,
+        seed=seed
     )
+    print(result)
+    if result == "You can`t create simulation with your parameters":
+        print(result)
+        current_date = datetime.today().strftime('%Y-%m-%d')
 
-    job_info_path = os.path.join(".", "flooddns", "runs", f"concurrent_jobs_{payload_data.num_jobs}",
-                                 f"{payload_data.num_cores}_core_failures", f"ring_size_{payload_data.ring_size}", "ecmp", "logs_floodns", "job_info.csv")
-    job_path = os.path.join(".", "flooddns", "runs", f"concurrent_jobs_{payload_data.num_jobs}",
-                            f"{payload_data.num_cores}_core_failures", f"ring_size_{payload_data.ring_size}", "ecmp")
+        simulation = {
+            "simulation_name": str(simulation_data.simulation_name),
+            # "datas":list(datas),
+            "path": "",
+            "date": current_date,
+            "params": str(payload_data.num_jobs) + "," + str(payload_data.n_cores) + "," + str(payload_data.ring_size) + "," + str(payload_data.routing),
+            "user_id": str(payload['user_id']),
+            "result": result
+        }
+        await insertSimulation(simulation)
+        simulations1 = await popSimulation(str(payload['user_id']))
+        return {"data": simulations1, "progress": result}
+
+    job_info_path = os.path.join(".", "flooddns", "runs", f"seed_{seed}", f"concurrent_jobs_{payload_data.num_jobs}",
+                                 f"{payload_data.n_cores}_core_failures", f"ring_size_{payload_data.ring_size}", payload_data.routing, "logs_floodns", "job_info.csv")
+    job_path = os.path.join(".", "flooddns", "runs", f"seed_{seed}", f"concurrent_jobs_{payload_data.num_jobs}",
+                            f"{payload_data.n_cores}_core_failures", f"ring_size_{payload_data.ring_size}", payload_data.routing)
     job_info_path = job_info_path.replace("\\", "/")
     job_path = job_path.replace("\\", "/")
     job_info = pd.read_csv(job_info_path, header=None)
@@ -63,37 +78,20 @@ async def run_simulation(request: Request):
                            "headers", "job_info.header")
     job_columns = pd.read_csv(job_csv)
     job_info.coulmns = job_columns.columns
-    if not job_info.empty:
-        datas = []
-        for j in range(len(job_info)):
-            data = {
-                "job_id": int(job_info.iloc[j, 0]),
-                "epoch": int(job_info.iloc[j, 1]),
-                "stage": int(job_info.iloc[j, 2]),
-                "start_time": int(job_info.iloc[j, 3]),
-                "end_time": int(job_info.iloc[j, 4]),
-                "duration": int(job_info.iloc[j, 5]),
-                "finished": job_info.iloc[j, 6],
-                "total_flows": int(job_info.iloc[j, 7]),
-                "flow_size": int(job_info.iloc[j, 8]),
-                "conn_ids": any(job_info.iloc[j, 9])
-            }
-            datas.append(data)
+    current_date = datetime.today().strftime('%Y-%m-%d')
 
-        current_date = datetime.today().strftime('%Y-%m-%d')
-
-        simulation = {
-            "simulation_name": str(simulation_data.simulation_name),
-            # "datas":list(datas),
-            "path": job_path,
-            "date": current_date,
-            "params": str(payload_data.num_jobs) + "," + str(payload_data.ring_size) + "," + str(payload_data.routing),
-            "user_id": str(payload['user_id']),
-            "result": result
-        }
-        await insertSimulation(simulation)
-        simulations1 = await popSimulation(str(payload['user_id']))
-        return {"data": simulations1, "progress": result}
+    simulation = {
+        "simulation_name": str(simulation_data.simulation_name),
+        # "datas":list(datas),
+        "path": job_path,
+        "date": current_date,
+        "params": str(payload_data.num_jobs) + "," + str(payload_data.n_cores) + "," + str(payload_data.ring_size) + "," + str(payload_data.routing),
+        "user_id": str(payload['user_id']),
+        "result": result
+    }
+    await insertSimulation(simulation)
+    simulations1 = await popSimulation(str(payload['user_id']))
+    return {"data": simulations1, "progress": result, "tempID": payload["params"]["tempID"]}
 
 
 @router.post("/get_simulate_flood_dns")
@@ -126,8 +124,78 @@ async def delte_simulation(request: Request):
 @router.post("/simulation_update")
 async def update_simulation(request: Request):
     payload = await request.json()
-    print(payload['simulation'][0]['simulation_id'])
-    await update_simulation_db(payload['params'], payload['simulation'][0]['simulation_id'], payload["user_id"])
+    seed = random.choice([0, 42, 200, 404, 1234])
+    payload_data = SimulationPayload(
+        simulation_name=payload['params']['simulation_name'],
+        num_jobs=payload['params']['num_jobs'],
+        num_tors=payload['params']['num_tors'],
+        n_cores=payload['params']['num_cores'],
+        ring_size=payload['params']['ring_size'],
+        routing=payload['params']['routing'],
+        path=payload['params']['path'],
+        seed=seed
+    )
+    simulation_data = TemporyPayload(
+        simulation_name=payload['params']['simulation_name'],
+        num_jobs=payload['params']['num_jobs'],
+        num_tors=payload['params']['num_tors'],
+        n_cores=payload['params']['num_cores'],
+        ring_size=payload['params']['ring_size'],
+        routing=payload['params']['routing'],
+        path=payload['params']['path'],
+        seed=seed
+    )
+    result = await local_run(
+        num_jobs=payload_data.num_jobs,
+        num_tors=payload_data.num_tors,
+        n_cores=payload_data.n_cores,
+        ring_size=payload_data.ring_size,
+        routing=payload_data.routing,
+        seed=seed
+    )
+    print(result)
+    if (result == "You can`t create simulation with your parameters"):
+        current_date = datetime.today().strftime('%Y-%m-%d')
+
+        simulation = {
+            "simulation_name": str(simulation_data.simulation_name),
+            # "datas":list(datas),
+            "path": "",
+            "date": current_date,
+            "params": str(payload_data.num_jobs) + "," + str(payload_data.n_cores) + "," + str(payload_data.ring_size) + "," + str(payload_data.routing),
+            "user_id": str(payload['user_id']),
+            "result": result
+        }
+        await update_simulation_db(simulation, payload['simulationID'], payload["user_id"])
+        simulations1 = await popSimulation(str(payload['user_id']))
+        return simulations1
+
+    job_info_path = os.path.join(".", "flooddns", "runs", f"seed_{seed}", f"concurrent_jobs_{payload_data.num_jobs}",
+                                 f"{payload_data.n_cores}_core_failures", f"ring_size_{payload_data.ring_size}", payload_data.routing, "logs_floodns", "job_info.csv")
+    job_path = os.path.join(".", "flooddns", "runs", f"seed_{seed}", f"concurrent_jobs_{payload_data.num_jobs}",
+                            f"{payload_data.n_cores}_core_failures", f"ring_size_{payload_data.ring_size}", payload_data.routing)
+    job_info_path = job_info_path.replace("\\", "/")
+    job_path = job_path.replace("\\", "/")
+    job_info = pd.read_csv(job_info_path, header=None)
+    if job_info.empty:
+        print("No jobs found.")
+        return
+    job_csv = os.path.join(".", "flooddns", "runs",
+                           "headers", "job_info.header")
+    job_columns = pd.read_csv(job_csv)
+    job_info.coulmns = job_columns.columns
+    current_date = datetime.today().strftime('%Y-%m-%d')
+
+    simulation = {
+        "simulation_name": str(simulation_data.simulation_name),
+        # "datas":list(datas),
+        "path": job_path,
+        "date": current_date,
+        "params": str(payload_data.num_jobs) + "," + str(payload_data.n_cores) + "," + str(payload_data.ring_size) + "," + str(payload_data.routing),
+        "user_id": str(payload['user_id']),
+        "result": result
+    }
+    await update_simulation_db(simulation, payload['simulationID'], payload["user_id"])
     simulations1 = await popSimulation(str(payload['user_id']))
     return simulations1
 
